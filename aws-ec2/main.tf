8c3d798717e3ad879a144b5f7e9d0b6766bc521d
Vishal Vijay <vishvj04@berkeley.edu>
2024-11-13 23:15:39 -0800

    initial terraform learning

---
+++ aws-ec2/main.tf
@@ -0,0 +1,14 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Define an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
